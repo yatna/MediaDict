a8d08b16624e3f8f3bdf0f41edf0a854a9e2fecc
Yatna <yatnavermaa@gmail.com>
2015-03-20 03:38:38 +0530

    fucntion added to view top 5 recent track by specific artist

--- Home.java
+++ Home.java
@@ -1,5 +1,6 @@
 package yatna.mediadict;
 import java.io.IOException;
+import java.util.Random;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -28,18 +29,20 @@ import android.widget.TabHost.TabSpec;
 
 public class Home extends Activity implements OnClickListener{
 	TabHost th;
-	TextView tv1;
+	TextView tv1,tv2;
 	HttpClient client;
 	String returned=" ";
-	String ultimate="";
-	String longitude="",latitude="",country="";
+	String ultimate="",mausam;
+	String longitude="",latitude="",country="",artist;
 	JSONObject json;
 	JSONArray arr;
 	Toast t;
 	static String URL= "http://www.telize.com/geoip";
 	static String URL2= "http://api.openweathermap.org/data/2.5/weather?";
+	static String URL3= "http://api.icndb.com/jokes/random?firstName=Rajnikant&lastName=";
+	static String URL4= "https://itunes.apple.com/search?";
 	
-	Button Music,Movies,TvSeries,Suggestion,ViewPayment;
+	Button Music,Movies,TvSeries,Suggestion,Joke;
 	TabSpec specs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,13 +70,14 @@ public class Home extends Activity implements OnClickListener{
          Movies=(Button)findViewById(R.id.b2);
          TvSeries=(Button)findViewById(R.id.b3);
         Suggestion=(Button)findViewById(R.id.b4);
-//         ViewPayment=(Button)findViewById(R.id.b5);
+        Joke=(Button)findViewById(R.id.b5);
          tv1=(TextView)findViewById(R.id.tv1);
+         tv2=(TextView)findViewById(R.id.tv2);
         Music.setOnClickListener(this);
  		Movies.setOnClickListener(this);
  		TvSeries.setOnClickListener(this);
  		Suggestion.setOnClickListener(this);
-// 		ViewPayment.setOnClickListener(this);
+		Joke.setOnClickListener(this);
     }
     
     
@@ -130,7 +134,7 @@ public class Home extends Activity implements OnClickListener{
 			longitude=result[0];
 			latitude=result[1];
 			country=result[2];
-			tv1.setText(result[2]);
+			
 		}
     }
     
@@ -169,10 +173,10 @@ public class Home extends Activity implements OnClickListener{
 
 				arr=lastTweet2("lat="+latitude+"&"+"lon="+longitude);
 
-				String s1=arr.getJSONObject(0).getString("main");
+			 mausam=arr.getJSONObject(0).getString("main");
 				String s2=arr.getJSONObject(0).getString("description");
 				
-				String[] ary={s1,s2};
+				String[] ary={mausam,s2};
 
 				return ary;
 				
@@ -194,7 +198,7 @@ public class Home extends Activity implements OnClickListener{
 
 		protected void onPostExecute(String[] result) {
 			
-			tv1.setText(result[0]+ "  "+ result[1]);
+			tv1.setText("You are currently at "+latitude +"latitude and "+longitude +" longitude.\nCurrently the weather is "+ result[1]);
 		}
     }
     
@@ -222,16 +226,141 @@ public class Home extends Activity implements OnClickListener{
 			startActivity(e);
 			break;
 		case R.id.b4:
-			
+			if(mausam.equals("Clouds"))
+				artist="Taylor Swift";
+			else if (mausam.equals("Clear"))
+				artist="Akon";
+			else if(mausam.equals("Rainy"))
+				artist="Linkin Park";
+			else
+				artist="Beyonce";
+			new Read4().execute("trackName","trackViewUrl");
 			break;
-//		case R.id.b5:
-//			Intent i = new Intent("com.second.VIEWPAYMENT");
-//			startActivity(i);
-//			break;
+		case R.id.b5:
+			new Read3().execute();
+			break;
 			
 		
 		}
 		
 	}
+    public JSONObject lastTweet3()throws ClientProtocolException,IOException,JSONException{
+		StringBuilder url= new StringBuilder(URL3);
+		HttpGet get =new HttpGet(url.toString());
+		HttpResponse r= client.execute(get);
+		int status=r.getStatusLine().getStatusCode();
+		if(status==200){
+			HttpEntity e=r.getEntity();
+			String data=EntityUtils.toString(e);
+
+     		JSONObject obj=new JSONObject(data);
+     		JSONObject timeline = obj.getJSONObject("value");
+     		return timeline;
+     		
+			
+			
+			
+			
+		}
+		else{
+			t=Toast.makeText(Home.this,"error",Toast.LENGTH_SHORT);
+			t.show();
+			return null;
+		}
+	}
+    public class Read3 extends AsyncTask<String, Integer, String>{
+
+		@Override
+		protected String doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			try {
+
+				json=lastTweet3();
+
+				String s1=json.getString("joke");
+				
+				return s1;
+				
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+				
+			} catch (JSONException e) {
+				
+				e.printStackTrace();
+				
+				
+			}
+			return null;
+		}
+
+		protected void onPostExecute(String result) {
+			
+			tv2.setText(result);
+		}
+    }
+    
+    public JSONObject lastTweet4(String username)throws ClientProtocolException,IOException,JSONException{
+		StringBuilder url= new StringBuilder(URL4);
+		url.append(username);
+		HttpGet get =new HttpGet(url.toString());
+		HttpResponse r= client.execute(get);
+		int status=r.getStatusLine().getStatusCode();
+		if(status==200){
+			HttpEntity e=r.getEntity();
+			String data=EntityUtils.toString(e);
+            JSONObject obj=new JSONObject(data);
+            JSONArray timeline = obj.getJSONArray("results");
+            Random ran = new Random();
+            int x = ran.nextInt(50);
+            return timeline.getJSONObject(x);
+     		
+			
+		}
+		else{
+			t=Toast.makeText(Home.this,"error",Toast.LENGTH_SHORT);
+			t.show();
+			return null;
+		}
+	}
+    public class Read4 extends AsyncTask<String, Integer, String[]>{
+
+		@Override
+		protected String[] doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			try {
+
+				json=lastTweet("term="+artist +"&entity=musicVideo");
+				String s1=json.getString("trackName");
+				String s2=json.getString("trackViewUrl");
+				
+				String[] arr={s1,s2};
+				return arr;
+
+				
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+				
+			} catch (JSONException e) {
+				e.printStackTrace();
+				
+				
+			}
+			return null;
+		}
+
+		protected void onPostExecute(String[] result) {
+			tv1.setText(result[0] + "    "+ result[1]);
+			
+		}
+    }
+    
+    
     
 }
